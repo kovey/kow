0734175da3a2a6364ffd4a9662afc951a4f17b5e
kovey <kovey@vip.qq.com>
2025-05-08 10:41:12 +0800

    fixed get parse data bugs

--- middleware/parse_request_data.go
+++ middleware/parse_request_data.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"net/http"
 	"strings"
 
 	"github.com/kovey/debug-go/debug"
@@ -21,28 +22,38 @@ func (p *ParseRequestData) Handle(ctx *context.Context) {
 		return
 	}
 
-	switch strings.ToLower(ctx.GetHeader(context.Content_Type_Key)) {
-	case context.Content_Type_Form:
+	if ctx.Request.Method != http.MethodPost && ctx.Request.Method != http.MethodPut && ctx.Request.Method != http.MethodPatch {
 		if err := ctx.ParseForm(ctx.ReqData); err != nil {
 			if err := result.ErrForm(ctx, result.Codes_Invalid_Params, err.Error()); err != nil {
 				debug.Erro("%s\n", err)
 			}
 			return
 		}
-	case context.Content_Type_Json:
-		if err := ctx.ParseJson(ctx.ReqData); err != nil {
-			if err := result.Err(ctx, result.Codes_Invalid_Params, err.Error()); err != nil {
-				debug.Erro("%s\n", err)
+	} else {
+		switch strings.ToLower(ctx.GetHeader(context.Content_Type_Key)) {
+		case context.Content_Type_Form:
+			if err := ctx.ParseForm(ctx.ReqData); err != nil {
+				if err := result.ErrForm(ctx, result.Codes_Invalid_Params, err.Error()); err != nil {
+					debug.Erro("%s\n", err)
+				}
+				return
 			}
-			return
-		}
-	case context.Content_Type_Xml:
-		if err := ctx.ParseXml(ctx.ReqData); err != nil {
-			if err := result.ErrXml(ctx, result.Codes_Invalid_Params, err.Error()); err != nil {
-				debug.Erro("%s\n", err)
+		case context.Content_Type_Json:
+			if err := ctx.ParseJson(ctx.ReqData); err != nil {
+				if err := result.Err(ctx, result.Codes_Invalid_Params, err.Error()); err != nil {
+					debug.Erro("%s\n", err)
+				}
+				return
 			}
-			return
+		case context.Content_Type_Xml:
+			if err := ctx.ParseXml(ctx.ReqData); err != nil {
+				if err := result.ErrXml(ctx, result.Codes_Invalid_Params, err.Error()); err != nil {
+					debug.Erro("%s\n", err)
+				}
+				return
+			}
 		}
 	}
+
 	ctx.Next()
 }
