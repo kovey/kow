cb98cacff74164b999906d3eddd29852f1a16126
kovey <kovey@vip.qq.com>
2025-10-11 17:48:21 +0800

    obtimzied safe_map

--- context/linked.go
+++ context/linked.go
@@ -10,55 +10,73 @@ func NewLinked[T comparable](k T) *Linked[T] {
 	return &Linked[T]{K: k, h: 1}
 }
 
-func (l *Linked[T]) Add(k T) {
-	l.h++
-	if l.Next == nil {
-		l.Next = NewLinked(k)
-		return
+func (l *Linked[T]) Add(k T) *Linked[T] {
+	if l.Empty() {
+		l.K = k
+		l.h++
+		return l
 	}
 
-	var next = l.Next
+	l.h++
+	var next = l
 	for next.Next != nil {
 		next = next.Next
 	}
 
 	next.Next = NewLinked(k)
+	return l
 }
 
-func (l *Linked[T]) Rem(k T) {
+func (l *Linked[T]) Rem(k T) *Linked[T] {
+	if l.Empty() {
+		return l
+	}
+
 	if l.K == k {
 		if l.Next != nil {
 			l.K = l.Next.K
 			l.Next = l.Next.Next
-			l.h--
 		}
-		return
+		l.h--
+		return l
 	}
 
 	var next = l.Next
 	var prev = l
-	for {
+	for next != nil {
 		if next.K == k {
 			prev.Next = next.Next
 			l.h--
-			return
+			return l
 		}
 
 		prev = next
 		next = next.Next
 	}
+
+	return l
+}
+
+func (l *Linked[T]) Empty() bool {
+	return l.h == 0
 }
 
 func (l *Linked[T]) Len() int {
 	return l.h
 }
 
-func (l *Linked[T]) Range(f func(k T)) {
+func (l *Linked[T]) Range(f func(k T)) *Linked[T] {
+	if l.Empty() {
+		return l
+	}
+
 	var next = l
 	for next != nil {
 		f(next.K)
 		next = next.Next
 	}
+
+	return l
 }
 
 func (l *Linked[T]) Values() []T {
--- context/linked_test.go
+++ context/linked_test.go
@@ -8,12 +8,9 @@ import (
 
 func TestLinked(t *testing.T) {
 	l := NewLinked(1)
-	l.Add(2)
-	l.Add(3)
-	l.Add(4)
-	l.Add(5)
-	l.Add(6)
-
+	l.Add(2).Add(3).Add(4).Add(5).Add(6)
+	assert.Equal(t, false, l.Empty())
+	assert.Equal(t, 6, l.Len())
 	assert.Equal(t, []int{1, 2, 3, 4, 5, 6}, l.Values())
 	l.Rem(3)
 	assert.Equal(t, []int{1, 2, 4, 5, 6}, l.Values())
@@ -23,4 +20,12 @@ func TestLinked(t *testing.T) {
 	assert.Equal(t, []int{2, 4, 5}, l.Values())
 	l.Add(7)
 	assert.Equal(t, []int{2, 4, 5, 7}, l.Values())
+	l.Rem(2).Rem(4).Rem(5).Rem(7).Rem(8)
+	assert.Equal(t, true, l.Empty())
+	assert.Equal(t, 0, l.Len())
+	assert.Equal(t, []int{}, l.Values())
+	l.Add(8)
+	assert.Equal(t, []int{8}, l.Values())
+	assert.Equal(t, false, l.Empty())
+	assert.Equal(t, 1, l.Len())
 }
